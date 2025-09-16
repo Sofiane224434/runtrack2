977fc798d8a7f37851bb2490ce4c49c64e6834a8
Azim.404 | Sofiane K <Sb.kherarfa.@gmail.com>
2025-09-16 15:57:42 +0200

    END job02

--- jour02/job01/index.php
+++ jour02/job01/index.php
@@ -1,9 +1,9 @@
 <?php
 
-for ($i = 1; $i <= 1137; $i++) {
+for ($i = 1; $i < 1137; $i++) {
     if ($i != 42) {
-        echo "<p>$i</p>";
+        echo "<p>$i</p>\n";
     } else {
-        echo "<p><b><i>$i</i></b></p>";
+        echo "<p><b><i>$i</i></b></p>\n";
     }
 }
