d1b04b3e77e9e24328dac3bf568933b7242dcd0b
Azim.404 | Sofiane K <Sb.kherarfa.@gmail.com>
2025-09-26 14:22:30 +0200

    END

--- jour09/job14/job14.sql
+++ jour09/job14/job14.sql
@@ -1,4 +1,4 @@
 USE jour09;
 SELECT prenom, nom, naissance
 FROM etudiants
-WHERE naissance >= '1998-01-01' AND naissance <= '2018-01-01';
+WHERE naissance >= '1998-01-01' AND naissance <= '2018-12-31';
